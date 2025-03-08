c5756db88fae77786da9c5aaa4dab5460b3b2c22
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-03-08 17:18:57 +0100

    Subscribed to the /odom topic

--- src/MPCtrlNode.py
+++ src/MPCtrlNode.py
@@ -1,10 +1,20 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String
+from nav_msgs.msg import Odometry
+from ackermann_msgs.msg import AckermannDriveStamped
 
 class MPCCtrlNode(Node):
     def __init__(self):
         super().__init__('mpc_controller')
+        
+        # Subscriber
+        self.subscription = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.odom_callback,
+            10)
+        self.subscription  # Prevent unused variable warning
         
         # Publisher
         self.publisher_ = self.create_publisher(String, 'chatter', 10)
@@ -12,22 +22,19 @@ class MPCCtrlNode(Node):
         # Timer to publish messages
         self.timer = self.create_timer(1.0, self.timer_callback)
         
-        # Subscriber
-        self.subscription = self.create_subscription(
-            String,
-            'chatter',
-            self.listener_callback,
-            10)
-        self.subscription  # Prevent unused variable warning
+        
     
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello, ROS 2!'
+        msg.data = 'Hello, This MPC Node!'
         self.publisher_.publish(msg)
         self.get_logger().info(f'Published: "{msg.data}"')
     
-    def listener_callback(self, msg):
-        self.get_logger().info(f'Received: "{msg.data}"')
+    def odom_callback(self, msg):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        v = msg.twist.twist.linear.x
+        theta = msg.pose.pose.orientation.z 
 
 
 def main(args=None):
