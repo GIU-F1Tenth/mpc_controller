7d2ce6d96878d5733dc07c254d22ff6592c5ea53
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-03-11 22:42:17 +0100

    Completed the Subscription Method

--- src/MPCtrlNode.py
+++ src/MPCtrlNode.py
@@ -1,41 +1,36 @@
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import String
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
+import numpy as np
+from MPC_Controller import MPC_Controller 
 
 class MPCCtrlNode(Node):
     def __init__(self):
         super().__init__('mpc_controller')
-        
-        # Subscriber
-        self.subscription = self.create_subscription(
-            Odometry,
-            '/odom',
-            self.odom_callback,
-            10)
-        self.subscription  # Prevent unused variable warning
-        
-        # Publisher
-        self.publisher_ = self.create_publisher(String, 'chatter', 10)
-        
-        # Timer to publish messages
-        self.timer = self.create_timer(1.0, self.timer_callback)
-        
-        
-    
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello, This MPC Node!'
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Published: "{msg.data}"')
-    
+
+        self.mpc = MPC_Controller(N = 10, T = 1.0, L = 0.33)
+
+        self.subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+
     def odom_callback(self, msg):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
         v = msg.twist.twist.linear.x
-        theta = msg.pose.pose.orientation.z 
+        theta = msg.pose.pose.orientation.z
 
+        # Create reference trajectory 
+        X_ref = np.tile([x, y, v, theta], (11, 1)).T  # (4, N+1) shape
+
+        optimal_accel, optimal_steer = self.mpc.solve_mpc(x, y, v, theta, X_ref)
+
+        # Publish Ackermann Drive command
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = v + optimal_accel * 0.1  
+        drive_msg.drive.steering_angle = optimal_steer
+        self.publisher_.publish(drive_msg)
 
 def main(args=None):
     rclpy.init(args=args)
